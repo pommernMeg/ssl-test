fbd1c219cfe7fdef6466597a5776eb0f871de0d3
Marcel Eggert <m.eggert@heinlein-support.de>
2021-05-03 19:12:01 +0200

    [CHANGE] added host infos

--- sslTester.py
+++ sslTester.py
@@ -7,10 +7,10 @@ class sslTester():
     """
    
     def __init__(self):
-        openssl_local_path = "/usr/local/bin/openssl"
-        openssl_local_version = "1.1.1k"
-        build_version = "0.0.1"
-        build_date = "03/05/2021"
+        self.openssl_local_path = "/usr/local/bin/openssl"
+        self.openssl_local_version = "1.1.1k"
+        self.build_version = "0.0.1"
+        self.build_date = "03/05/2021"
 
     def separatorLine(self):
         print(f"####################################################################")
@@ -39,19 +39,47 @@ class sslTester():
         response = urllib.request.urlopen(req)
         data = response.read()
         values = json.loads(data) 
-
-        print(f"""#
+        
+        path, version, info = self.check_openssl_version()
+        if len(info) == 0:
+            print(f"""#
 #  IPv4: {values['ipinfo'][1]['ip']} \t IPv6: {values['ipinfo'][0]['ip']} 
 #  hostname:  {socket.gethostname()}             
 #  
-#  openssl path: {dd} \topenssl version: {ss} 
+#  OpenSSL 
+#  path: {path} \tversion: {version} 
 #  """)
-        self.separatorLine()
+            self.separatorLine()
+        else:
+            print(f"""#
+#  IPv4: {values['ipinfo'][1]['ip']} \t IPv6: {values['ipinfo'][0]['ip']} 
+#  hostname:  {socket.gethostname()}             
+#  
+#  OpenSSL 
+#  path: {path} \tversion: {version} 
+#
+#  {info}
+#  """)
+            self.separatorLine()
     
     def check_openssl_version(self):
-        import subprocess
+        from subprocess import check_output
+        from packaging.version import Version, LegacyVersion
+        import re
         
-        subprocess.call(["ls", "-l"])
+        path = str(check_output(["which", "openssl"]).decode("utf-8")).replace("\n","")
+        
+        version = str(check_output([path, "version"]).decode("utf-8")).replace("\n","")
+        
+        vers1 = re.sub("[^0-9.]+", "", version.split(" ")[1])
+        vers2 = re.sub("[^0-9.]+", "", self.openssl_local_version)
+        
+        if Version(vers1) < Version(vers2):
+            info = "Bitte Version aus lib Verzeichnis nutzen"
+        else:
+            info = ""
+            
+        return path, version, info
         
     def loadConfig(self):
         var =""
